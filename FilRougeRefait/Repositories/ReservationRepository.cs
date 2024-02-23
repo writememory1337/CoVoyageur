72bc5e4fa03e4092b86dc8d08911611a48688d01
writememory1337 <ordiplus58@outlook.fr>
2024-02-23 16:19:21 +0100

    feat: added reserv repo

--- FilRougeRefait/Repositories/ReservationRepository.cs
+++ FilRougeRefait/Repositories/ReservationRepository.cs
@@ -1,52 +1,74 @@
 ﻿using CoVoyageur.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 using CoVoyageur.API.Data;
-using CoVoyageur.API.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace CoVoyageur.API.Repositories
 {
     public class ReservationRepository : IRepository<Reservation>
     {
-        private ApplicationDbContext _db { get; }
+        private readonly ApplicationDbContext _db;
+
         public ReservationRepository(ApplicationDbContext db)
         {
             _db = db;
         }
 
-        public Task<List<Reservation>> GetAll()
+        public async Task<List<Reservation>> GetAll()
         {
-            throw new NotImplementedException();
+            return await _db.Reservations.ToListAsync();
         }
 
-        public Task<List<Reservation>> GetAll(Expression<Func<Reservation, bool>> predicate)
+        public async Task<List<Reservation>> GetAll(Expression<Func<Reservation, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return await _db.Reservations.Where(predicate).ToListAsync();
         }
 
-        public Task<Reservation?> GetById(int id)
+        public async Task<Reservation?> GetById(int id)
         {
-            throw new NotImplementedException();
+            return await _db.Reservations.FindAsync(id);
         }
 
-        public Task<Reservation?> Get(Expression<Func<Reservation, bool>> predicate)
+        public async Task<Reservation?> Get(Expression<Func<Reservation, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return await _db.Reservations.FirstOrDefaultAsync(predicate);
         }
 
-        public Task<Reservation?> Add(Reservation entity)
+        public async Task<Reservation?> Add(Reservation reservation)
         {
-            throw new NotImplementedException();
+            var added = await _db.Reservations.AddAsync(reservation);
+            await _db.SaveChangesAsync();
+            return added.Entity;
         }
 
-        public Task<Reservation?> Update(Reservation entity)
+        public async Task<Reservation?> Update(Reservation reservation)
         {
-            throw new NotImplementedException();
+            var reservationDb = await GetById(reservation.ID);
+            if (reservationDb == null)
+                return null;
+
+            reservationDb.ID_Ride = reservation.ID_Ride;
+            reservationDb.ID_Passenger = reservation.ID_Passenger;
+            reservationDb.Statuts = reservation.Statuts;
+            reservationDb.Travel = reservation.Travel;
+            reservationDb.Passenger = reservation.Passenger;
+
+            await _db.SaveChangesAsync();
+            return reservationDb;
         }
 
-        public Task<bool> Delete(int id)
+        public async Task<bool> Delete(int id)
         {
-            throw new NotImplementedException();
+            var reservation = await GetById(id);
+            if (reservation == null)
+                return false;
+
+            _db.Reservations.Remove(reservation);
+            return await _db.SaveChangesAsync() > 0;
         }
     }
 }
